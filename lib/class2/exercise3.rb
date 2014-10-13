4a37c230bf663bc7041a7131cfb63e333c9f492d
Rachel Routson <roccosummer@gmail.com>
2014-10-12 19:27:00 -0700

    completed ex 3

--- lib/class2/exercise3.rb
+++ lib/class2/exercise3.rb
@@ -2,3 +2,9 @@
 #
 # If a person is 1,160 million seconds old, display their age in years. As
 # always, assume leap years don't exist.
+
+years_old = 1_160_000_000
+
+seconds_per_year = 86_400
+
+puts years_old / seconds_per_year / 365
