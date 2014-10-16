e1d53405f33781d84688bbee08136bf59b7d0445
Rachel Routson <roccosummer@gmail.com>
2014-10-15 18:51:05 -0700

    completed ex 1

--- scratch.rb
+++ scratch.rb
@@ -1,2 +1,21 @@
 # The secret of getting ahead is getting started.
 # - Mark Twain
+puts 5**2
+puts 5**0.5
+puts 7/3
+puts 7%3
+puts 365%7
+
+puts (5-2).abs
+puts (2-5).abs
+
+puts rand
+puts rand
+puts rand
+puts(rand(100))
+puts(rand(999999999999999999999))
+
+puts(Math::PI)
+puts(Math::E)
+puts(Math.cos(Math::PI/3))
+
