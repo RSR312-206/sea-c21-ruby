8c8800efaaffd2dfc8ed53eb1c37ac36aecbd5cd
Rachel Routson <roccosummer@gmail.com>
2014-11-02 16:49:20 -0800

    finished class8 ex 1 -4

--- lib/class4/exercise4.rb
+++ lib/class4/exercise4.rb
@@ -29,22 +29,18 @@
 # rubocop:disable MethodLength
 
 def old_school_roman_numeral(num)
+  conversion = ''
 
-  roman_hash = {
-                 1000 => "M",
-                 500  => "D",
-                 100  => "C",
-                 50   => "L",
-                 10   => "X",
-                 5    => "V",
-                 1    => "I"
-               }
+  roman_hash = { 1000 => 'M', 500 => 'D', 100 => 'C', 50 => 'L', 10 => 'X', \
+                 5 => 'V', 1 => 'I' }
+
   roman_hash.keys.each do |key|
     result = num / key
     if result > 0
     puts roman_hash[key] * result
     num = num - (key * result)
   end
+
 end
 
 input = ARGV[0].to_i
@@ -53,4 +49,3 @@ abort 'Usage: exercise4.rb [1-1000]' unless input.between?(1, 1000)
 
 puts old_school_roman_numeral(input)
 end
-
